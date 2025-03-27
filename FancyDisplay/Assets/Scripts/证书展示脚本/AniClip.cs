256c2bb914ab3b360d13172a162caed298094329
bility <292340644@qq.com>
2025-03-27 18:11:23 +0800

    1

--- FancyDisplay/Assets/Scripts/证书展示脚本/AniClip.cs
+++ FancyDisplay/Assets/Scripts/证书展示脚本/AniClip.cs
@@ -11,6 +11,10 @@ public class AniClip : MonoBehaviour
     MeshRenderer frontRender;
     Material material;
 
+    [SerializeField]
+    MeshRenderer backRender;
+    Material material_back;
+
     public float clip = 0f;
 
     public float A = 1f;
@@ -18,7 +22,16 @@ public class AniClip : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        material = frontRender.material;
+        if (frontRender != null)
+        {
+            material = frontRender.material;
+        }
+
+        if (backRender != null)
+        {
+            material_back = backRender.material;
+        }
+
     }
 
     // Update is called once per frame
@@ -26,10 +39,13 @@ public class AniClip : MonoBehaviour
     {
         //应用溶解进度
         AdvancedDissolveProperties.Cutout.Standard.UpdateLocalProperty(material, AdvancedDissolveProperties.Cutout.Standard.Property.Clip, clip);
-
-        // 应用透明度
-        Color color = material.color;
-        color.a = A;
-        material.color = color;
+        AdvancedDissolveProperties.Cutout.Standard.UpdateLocalProperty(material_back, AdvancedDissolveProperties.Cutout.Standard.Property.Clip, clip);
+        //应用透明度
+        Color color1 = material.color;
+        color1.a = A;
+        if (material != null)
+        {
+            material.color = color1;
+        }
     }
 }
