9f97235bd5329768e7e44d8e2baeafdba7c36f44
bility <292340644@qq.com>
2025-11-02 23:35:41 +0800

    animator

---
+++ ScreenTouch2_HDRP/Assets/AniSwitch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AniSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+    private bool _isOpen = false;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        // 初始化状态（确保参数与默认状态一致）
+        _animator.SetBool("IsOpen", _isOpen);
+    }
+
+    void Update()
+    {
+        // 按 E 键直接切换状态，无需锁定（中途可反向）
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _isOpen = !_isOpen;
+            _animator.SetBool("IsOpen", _isOpen); // 实时更新参数，触发反向过渡
+        }
+    }
+}
