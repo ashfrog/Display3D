2474438f7eab8a82ef569d36101b68b27d3e858f
bility <292340644@qq.com>
2025-11-05 16:05:04 +0800

    ReflectionProbe反射球

---
+++ ScreenTouch2_HDRP/Assets/Ins.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ins : MonoBehaviour
+{
+    /// <summary>
+    /// 底层UI
+    /// </summary>
+    public Camera UICamera;
+
+    /// <summary>
+    /// 顶层UI
+    /// </summary>
+    public Camera UICamera_Top;
+
+    /// <summary>
+    /// 3D相机
+    /// </summary>
+    public Camera mainCamera;
+
+    /// <summary>
+    /// 生成实例
+    /// </summary>
+    public GameObject objChair;
+
+    public List<Canvas> uiCanvas;
+
+    public SmoothOrbitManipulator SmoothOrbitManipulator;
+
+
+}
--- ScreenTouch2_HDRP/Assets/LRLayerSet.cs
+++ ScreenTouch2_HDRP/Assets/LRLayerSet.cs
@@ -1,13 +1,41 @@
 using UnityEngine;
 
+/// <summary>
+/// 实例化一个Ins到Display2中
+/// </summary>
 public class LRLayerSet : MonoBehaviour
 {
-    public LayerMask leftLayerMask;
+    public Ins defaultIns;
+    private Ins rightIns;
+    public LayerMask rightLayerMask;
+    public int rightLayer = 11;
+    public int rightDisplay = 1;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rightIns = Instantiate(defaultIns);
+        rightIns.SmoothOrbitManipulator.leftEnable = false;
+        SetLayerRecursively(rightIns.objChair, rightLayer);
+        rightIns.UICamera.targetDisplay = rightDisplay;
+        rightIns.UICamera_Top.targetDisplay = rightIns.UICamera.targetDisplay;
+        rightIns.mainCamera.targetDisplay = rightDisplay;
+        rightIns.mainCamera.cullingMask = rightLayerMask;
+    }
 
+    /// <summary>
+    /// 递归设置物体及其子物体的Layer
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="layer"></param>
+    static void SetLayerRecursively(GameObject obj, int layer)
+    {
+        obj.layer = layer;
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, layer);
+        }
     }
 
     // Update is called once per frame
