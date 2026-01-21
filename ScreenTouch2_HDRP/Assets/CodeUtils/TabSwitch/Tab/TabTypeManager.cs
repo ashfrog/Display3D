5a68fb2749ad7ed7571d1fe6e9221172139ba6ea
bility <292340644@qq.com>
2026-01-21 10:54:44 +0800

    1

--- ScreenTouch2_HDRP/Assets/CodeUtils/TabSwitch/Tab/TabTypeManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "TabSwitcher/TabTypeManager")]
-public class TabTypeManager : ScriptableObject
-{
-    [Tooltip("Tab类型名称列表（类似enum，可随时增加/删除）")]
-    public List<string> tabTypes = new List<string>();
-}
